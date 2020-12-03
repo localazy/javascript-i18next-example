20f5f51ced00b73fe3355628fbfb2d4086e6cb70
Jan Bílek <honza@elision.design>
2020-12-03 11:18:20 +0100

    Add basic i18next config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-document.querySelector("#app").innerHTML = `Localize JS app with Localazy`
+import i18next from "i18next";
+import enJson from "./locales/en.json";
+
+i18next.init({
+  lng: 'en',
+  debug: true,
+  resources: {
+    en: {
+      translation: enJson,
+    },
+  }
+}, function(err, t) {    
+  document.querySelector("#app").innerHTML = i18next.t('fields');
+});
