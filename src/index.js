a1787dfab7b362d77844f68e07aa3e89cdc17f63
Jan Bílek <honza@elision.design>
2020-12-04 12:39:04 +0100

    Add supported languages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import i18next from "i18next";
+import LocalazyMeta from './localazy-meta';
 import enJson from "./locales/en.json";
+import frJson from "./locales/fr.json";
+
+const getSupportedLangs = ()=>{
+  return LocalazyMeta.languages.map(l=>l.language)
+}
+
+
+console.log(getSupportedLangs())
 
 i18next.init({
   lng: 'en',
   debug: true,
-  resources: {
+  supportedLngs: getSupportedLangs(),
+  resources:{
     en: {
       translation: enJson,
     },
-  }
-}, function(err, t) {    
+    fr: {
+      translation: frJson,
+    },    
+  },
+}, function (err, t) {
   document.querySelector("#app").innerHTML = i18next.t('fields');
 });
