3437b689fec56137b47b3f1a972e09c4a14d3939
Jan Bílek <honza@elision.design>
2020-12-06 11:14:54 +0100

    Resolve plurals function

--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ import i18next from "i18next";
 import LocalazyMeta from './localazy-meta';
 import enJson from "./locales/en.json";
 import frJson from "./locales/fr.json";
+import csJson from "./locales/cs.json";
 
 const getSupportedLangs = () => {
   return LocalazyMeta.languages.map(l => l.language)
 }
 
+const getKeyPlural = (key, count) => {
+  const currentLanguage = LocalazyMeta.languages.find(l => l.language === i18next.language);
+  const pluralType = currentLanguage.pluralType(count);
+  return `${key}.${pluralType}`;
+}
+
+const p = getKeyPlural;
+
 const updateHtmlContent = () => {
-  document.querySelector("#app").innerHTML = i18next.t('fields');
+  document.querySelector("#app").innerHTML = i18next.t(p('calendar', 2));
 }
 
 i18next.init({
-  lng: 'en',
+  lng: 'cs',
   fallbackLng: 'en',
   debug: true,
   supportedLngs: getSupportedLangs(),
@@ -22,6 +31,9 @@ i18next.init({
     },
     fr: {
       translation: frJson,
+    },
+    cs: {
+      translation: csJson,
     },
   },
 }, function (err, t) {
