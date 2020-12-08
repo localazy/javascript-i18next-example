7d52ed2f94006ca25bcf2e16b71f174c652d853a
Jan Bílek <honza@elision.design>
2020-12-08 11:46:20 +0100

    Implement language switcher

--- src/index.js
+++ src/index.js
@@ -1,42 +1,31 @@
-import { i18n, initI18n, getLanguages, getCurrentLanguage, getKeyPlural as p } from "./i18n";
-
-let count = 0;
+import { i18n, initI18n, getLanguages, getCurrentLanguage } from "./i18n";
 
 const createLanguageSelector = () => {
   let template = '<select id="selector">';
   getLanguages().forEach(l => {
-    template += `<option ${l.language === getCurrentLanguage()?'selected':''} value="${l.language}">${l.localizedName}</option>`
+    template += `
+    <option ${l.language === getCurrentLanguage() ? "selected" : ""} value="${l.language}">
+      ${l.localizedName}
+    </option>`
   })
   template += '</select>';
   return template;
 }
 
 const getTranslatedContent = () => {
-  return `${count}  ${i18n.t(p('calendar', count))}<br>
-          ${count}  ${i18n.t(p('field', count))}<br>
-          ${count}  ${i18n.t(p('event', count))}<br>
-          ${count}  ${i18n.t(p('title', count))}<br>
-          ${count}  ${i18n.t(p('color', count))}`;
+  return i18n.t("hello_localazy");
 }
 
 const updateTranslatedContent = () => {
   document.querySelector("#content").innerHTML = getTranslatedContent();
 }
 
-const createPageContent = () => {
+const initPageContent = () => {
   document.querySelector("#app").innerHTML = `  
-  ${createLanguageSelector()} 
-  <input id="count" type="number" placeholder="count" value="${count}" min="0"/>
+  ${createLanguageSelector()}   
   <div id="content">  
     ${getTranslatedContent()}
   </div>`;
-
-  document.querySelector("#count").addEventListener("input", (e) => {
-    if (e.target.value) {
-      count = e.target.value;
-      updateTranslatedContent();
-    }
-  })
 
   document.querySelector("#selector").addEventListener("change", (e) => {
     i18n.changeLanguage(e.target.value);
@@ -44,4 +33,4 @@ const createPageContent = () => {
   })
 }
 
-initI18n(createPageContent);
+initI18n(initPageContent);
